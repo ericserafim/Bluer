af04880b7ea656819e4fefaaa302d873ffbd412a
Eric Serafim <ericserafim@gmail.com>
2021-04-25 23:05:10 -0300

    Increased watch dog hotend to 60 seconds

--- src/Marlin/Configuration.h
+++ src/Marlin/Configuration.h
@@ -1507,7 +1507,7 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 195
+#define PREHEAT_1_TEMP_HOTEND 190
 #define PREHEAT_1_TEMP_BED     55
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
